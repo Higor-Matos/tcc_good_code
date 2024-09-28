4a680c5e78ad60465157214e7155c6252481518a
Higor de Deus <higordeus22@gmail.com>
2024-09-28 20:49:36 -0300

    Feat(Routes): Swagger docs, routes, guinicorn

--- app/presentation/app.py
+++ app/presentation/app.py
@@ -3,7 +3,7 @@
 import os
 
 from flasgger import Swagger
-from flask import Flask, g
+from flask import Flask, g, redirect, url_for
 from injector import Injector
 from sqlalchemy.orm import scoped_session, sessionmaker
 
@@ -19,30 +19,40 @@ logger = setup_logger()
 
 app = Flask(__name__)
 
-swagger_config = {
-    "headers": [],
-    "specs": [
-        {
-            "endpoint": "apispec",
-            "route": "/apispec.json",
-            "rule_filter": lambda rule: True,
-            "model_filter": lambda tag: True,
-        }
-    ],
-    "static_url_path": "/flasgger_static",
-    "swagger_ui": True,
-    "specs_route": "/swagger/",
-}
 
-swagger_template = {
-    "info": {
-        "title": "API de Exemplo",
-        "description": "Documentação da API de Exemplo utilizando Flasgger",
-        "version": "1.0.0",
+def configure_swagger(app: Flask) -> None:
+    """Configura o Swagger para o aplicativo Flask com informações sobre o TCC."""
+    swagger_config = {
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": "apispec",
+                "route": "/apispec.json",
+                "rule_filter": lambda rule: True,
+                "model_filter": lambda tag: True,
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs/",  # Alterado para servir a documentação em /apidocs
     }
-}
 
-Swagger(app, config=swagger_config, template=swagger_template)
+    swagger_template = {
+        "info": {
+            "title": "API TCC - Clean Code",
+            "description": (
+                "Documentação da API de exemplo para o TCC: 'Impacto da Implementação de Clean Code nas "
+                "Fases Iniciais de Desenvolvimento de Software'. "
+                "Esta API demonstra boas práticas de código limpo e organizado."
+            ),
+            "version": "1.0.0",
+        }
+    }
+
+    Swagger(app, config=swagger_config, template=swagger_template)
+
+
+configure_swagger(app)
 
 init_db()
 logger.info("Banco de dados inicializado.")
@@ -75,8 +85,14 @@ def teardown_request(exception=None) -> None:
         logger.warning("Nenhuma sessão encontrada para encerrar.")
 
 
+@app.route("/")
+def index():
+    """Redireciona a rota padrão para a documentação do Swagger em /apidocs."""
+    return redirect(url_for("flasgger.apidocs"))
+
+
 if __name__ == "__main__":
-    app_name = os.getenv("APP_NAME", "Flask App")
+    app_name = os.getenv("APP_NAME", "TCC API - Clean Code")
     app_port = int(os.getenv("APP_PORT", "5000"))
     logger.info("Iniciando servidor %s na porta %d.", app_name, app_port)
     app.run(port=app_port, use_reloader=False)
